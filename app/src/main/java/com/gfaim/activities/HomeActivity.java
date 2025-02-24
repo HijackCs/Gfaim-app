1d0022b88875358e147f328fdb7b7c37255ecc82
yann.cornet <yann_cornet@ens.univ-artois.fr>
2025-02-24 15:29:45 +0100

    Ajout du rond pour aller au profile

--- app/src/main/java/com/gfaim/activities/HomeActivity.java
+++ app/src/main/java/com/gfaim/activities/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.gfaim.activities;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,6 +33,13 @@ public class HomeActivity extends AppCompatActivity {
             });
             new NavigationBar(this);
             setupCarrousel();
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         } catch (Exception e) {
             log.warning("[acceuil][onCreate] Problème au lancement de HomeActivity");
         }

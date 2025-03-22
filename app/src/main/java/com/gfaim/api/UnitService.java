1de3721e8b3cf3fe833393260816264b496a94e2
yann.cornet <yann_cornet@ens.univ-artois.fr>
2025-03-22 17:41:49 +0100

    Change gradle

--- app/src/main/java/com/gfaim/api/UnitService.java
+++ app/src/main/java/com/gfaim/api/UnitService.java
@@ -4,6 +4,15 @@ import com.gfaim.activities.calendar.model.Unit;
 
 import java.util.List;
 
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Path;
+
 public class UnitService {
     @GET("/units")
     Call<List<Unit>> getUnits(@Header("Authorization") String token);
